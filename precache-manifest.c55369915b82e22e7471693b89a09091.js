529d3cb6daa42f0824363a1f8247bdf6089396e6
melon <meloniusz@hotmail.com>
2020-05-07 22:53:49 +0800

    Updates

---
+++ precache-manifest.c55369915b82e22e7471693b89a09091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6db287b49681ba406a7e1320adf18e",
+    "url": "/covid-19-global-tracker/index.html"
+  },
+  {
+    "revision": "9acd181466eb327052f1",
+    "url": "/covid-19-global-tracker/static/css/main.8ef102e2.chunk.css"
+  },
+  {
+    "revision": "a63ab9cac43b138a8944",
+    "url": "/covid-19-global-tracker/static/js/2.35266ea7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-global-tracker/static/js/2.35266ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acd181466eb327052f1",
+    "url": "/covid-19-global-tracker/static/js/main.9158078e.chunk.js"
+  },
+  {
+    "revision": "3f793f2c1121dfbe3851",
+    "url": "/covid-19-global-tracker/static/js/runtime-main.739dfe4c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-global-tracker/static/media/image.d7265326.png"
+  }
+]);
