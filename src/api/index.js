f941569b7a59302efd8d5bc01ffe84a5f13e975f
melon <meloniusz@hotmail.com>
2020-05-07 21:40:27 +0800

    populated option box with 187 countries

--- src/api/index.js
+++ src/api/index.js
@@ -26,11 +26,13 @@ export const fetchDailyData = async () => {
   } catch (error) {}
 };
 
-export const countries = async () => {
+export const fetchCountries = async () => {
   try {
-    const response = await axios.get(`${url}/countries`);
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
 
-    console.log(response);
+    return countries.map((country) => country.name);
   } catch (error) {
     console.log(error);
   }
